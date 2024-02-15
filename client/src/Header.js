8e3e3bf14b84361996f663a800c979ceaac3ea66
sfga12 <pixelbey12@gmail.com>
2024-02-15 20:05:21 +0300

    backend router update

---
+++ client/src/Header.js
@@ -0,0 +1,24 @@
+import logo from './assets/socialgram.svg';
+import icon_bookmarks from './assets/bookmarks-logo-navbar.svg';
+import icon_home from './assets/home-logo-navbar.svg';
+import icon_newpost from './assets/newpost-logo-navbar.svg';
+import icon_trends from './assets/trends-logo-navbar.svg';
+import {Link} from "react-router-dom";
+export default function Header() {
+    return(
+        <header>
+        <div className="header_background"></div>
+        <Link to="/" className="logo">
+          <img src={logo} alt="" />
+        </Link>
+        <nav>
+          <Link to="/"> <i><img src={icon_home} alt="" /></i> <span>Home</span> </Link>
+          <a href=""><i><img src={icon_newpost} alt="" /></i><span>New Post</span></a>
+          <a href=""><i><img src={icon_bookmarks} alt="" /></i> <span>Bookmarks</span> </a>
+          <a href=""><i><img src={icon_trends} alt="" /></i> <span>Trends</span></a>
+          <Link to=""><span>Register</span></Link>
+          <Link to="/login"><span>Login</span></Link>
+        </nav>
+      </header>
+    );
+}
---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,13 @@
+import Post from "../Post";
+
+export default function IndexPage() {
+    return(
+        <div className="posts">
+        <Post/>
+        <Post/>
+        <Post/>
+
+       </div> 
+    );
+
+}
